1a317d91d27349403a8cfde88624610296ed3212
Anderson Rocha <andersonecomp091@gmail.com>
2020-05-19 15:48:09 -0300

    Corrigindo problemas com o plugin para filtragem de imagens baseado na tag que definimos anteriormente

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -1,5 +1,13 @@
 import $ from "jquery";
 
+const loadHtmlSuccessCallbacks = [];
+
+export function onLoadHtmlSuccess(callback) {
+  if (!loadHtmlSuccessCallbacks.includes(callback)) {
+    loadHtmlSuccessCallbacks.push(callback);
+  }
+}
+
 function loadIncludes(parent) {
   if (!parent) parent = "body";
 
@@ -12,6 +20,7 @@ function loadIncludes(parent) {
         success(data) {
           $(item).html(data);
           $(item).removeAttr("wm-include");
+          loadHtmlSuccessCallbacks.forEach(callback => callback(data));
           loadIncludes(item);
         },
       });
